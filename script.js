0978d2d65f9b507e452574dd980aa60581d4cc9a
Michal <m1ch4l88@gmail.com>
2017-05-23 06:26:01 +0200

    Map styling improved

--- script.js
+++ script.js
@@ -7,6 +7,14 @@ var cellSize = 20;
 var cellXCount = width / cellSize;
 var cellYCount = height / cellSize;
 
+var mouseOverCanvas = false;
+
+var mouseX;
+var mouseY;
+var cellX;
+var cellY;
+var elementRect;
+
 var canvas;
 var ctx;
 
@@ -43,6 +51,19 @@ var infoRGB = 100;
 var increaseColor = true;
 
 
+// colors
+var backgroundColor = '#eee';
+var gridColor = '#bbb';
+var obstaclesColor = '#555';
+var startPointColor = '#3F9FBF';
+var finishPointColor = '#D14343';
+var pathColor = '#555';
+var openElementColor = '#AED7E6';
+var closedElementColor = '#9BADB3';
+var currentElementColor = '#80BA45';
+var pointerColor = '#666';
+
+
 //--------------------------------------------------------------------//
 
 $(document).ready(function () {
@@ -53,14 +74,22 @@ $(document).ready(function () {
 	
 	var interval = setInterval(renderFrame, 40);
 	
+	$('#canvas').on('mouseenter', function() {
+		mouseOverCanvas = true;
+	});
+	
+	$('#canvas').on('mouseleave', function() {
+		mouseOverCanvas = false;
+	});
+	
 	$('#canvas').on('mousedown', function(e) {
 		if(!searching) {
 			mousePressed = true;
-			var elementRect = canvas.getBoundingClientRect();
-			var mouseX = e.pageX - elementRect.x;
-			var mouseY = e.pageY - elementRect.y;
-			var cellX = Math.floor(mouseX / cellSize);
-			var cellY = Math.floor(mouseY / cellSize);
+			elementRect = canvas.getBoundingClientRect();
+			mouseX = e.pageX - elementRect.x;
+			mouseY = e.pageY - elementRect.y;
+			cellX = Math.floor(mouseX / cellSize);
+			cellY = Math.floor(mouseY / cellSize);
 			
 			if (settingStartPoint && matrix[cellY][cellX] !== 'F') {
 				matrix[startY][startX] = 0;
@@ -111,12 +140,13 @@ $(document).ready(function () {
 	});
 		
 	$('#canvas').on('mousemove', function(e) {
+		elementRect = canvas.getBoundingClientRect();
+		mouseX = e.pageX - elementRect.x;
+		mouseY = e.pageY - elementRect.y;
+		cellX = Math.floor(mouseX / cellSize);
+		cellY = Math.floor(mouseY / cellSize);
+		
 		if(mousePressed) {
-			var elementRect = canvas.getBoundingClientRect();
-			var mouseX = e.pageX - elementRect.x;
-			var mouseY = e.pageY - elementRect.y;
-			var cellX = Math.floor(mouseX / cellSize);
-			var cellY = Math.floor(mouseY / cellSize);
 			if (matrix[cellY][cellX] === 0 && drawingMode) {
 				matrix[cellY][cellX] = 1;
 			} else if (matrix[cellY][cellX] === 1 && !drawingMode) {
@@ -151,31 +181,41 @@ $(document).ready(function () {
 		$('#removeOption').removeClass('activeOption');
 		$('#drawOption').addClass('activeOption');
 		drawingMode = true;
+		
+		if(disableDrawing && !searching) {
+			clearInterval(interval);
+			interval = setInterval(renderFrame, 40);
+		}
 	});
 	
 	$('#removeOption').on('click', function() {
 		$('#drawOption').removeClass('activeOption');
 		$('#removeOption').addClass('activeOption');
 		drawingMode = false;
+		
+		if(disableDrawing && !searching) {
+			clearInterval(interval);
+			interval = setInterval(renderFrame, 40);
+		}
 	});
 	
 	
 	$('#slowOption').on('click', function() {
 		$('.speedOptionDiv').removeClass('activeOption');
 		$('#slowOption').addClass('activeOption');
-		animationSpeed = 500;
+		animationSpeed = 1000;
 	});
 	
 	$('#mediumOption').on('click', function() {
 		$('.speedOptionDiv').removeClass('activeOption');
 		$('#mediumOption').addClass('activeOption');
-		animationSpeed = 50;
+		animationSpeed = 100;
 	});
 	
 	$('#fastOption').on('click', function() {
 		$('.speedOptionDiv').removeClass('activeOption');
 		$('#fastOption').addClass('activeOption');
-		animationSpeed = 5;
+		animationSpeed = 3;
 	});
 	
 	
@@ -191,7 +231,13 @@ $(document).ready(function () {
 		$('#findButton').addClass('disabledButton');
 		$('#setFinishButton').attr('disabled', true);
 		$('#setFinishButton').addClass('disabledButton');
+		
+		if(disableDrawing) {
+			clearInterval(interval);
+			interval = setInterval(renderFrame, 40);
+		}
 	});
+	
 	
 	$('#setFinishButton').on('click', function() {
 		settingFinishPoint = true;
@@ -203,6 +249,11 @@ $(document).ready(function () {
 		$('#findButton').addClass('disabledButton');
 		$('#setStartButton').attr('disabled', true);
 		$('#setStartButton').addClass('disabledButton');
+		
+		if(disableDrawing) {
+			clearInterval(interval);
+			interval = setInterval(renderFrame, 40);
+		}
 	});
 	
 	$('#resetButton').on('click', function(){
@@ -334,8 +385,8 @@ $(document).ready(function () {
 
 
 function renderFrame() {
-	ctx.fillStyle = '#222';
-	ctx.strokeStyle = '#ddd';
+	ctx.fillStyle = obstaclesColor;
+	ctx.strokeStyle = gridColor;
 	ctx.clearRect(0, 0, width, height);
 	
 	for (var y = 0; y < cellYCount; y++) {
@@ -343,18 +394,36 @@ function renderFrame() {
 			if (matrix[y][x] === 0) {
 				ctx.strokeRect(x*cellSize, y*cellSize, cellSize, cellSize);
 			} else if (matrix[y][x] === 'S') {		
-				ctx.fillStyle = '#00d';
+				ctx.fillStyle = startPointColor;
 				ctx.fillRect(x*cellSize, y*cellSize, cellSize, cellSize);
-				ctx.fillStyle = '#222';
+				ctx.fillStyle = obstaclesColor;
 			} else if (matrix[y][x] === 'F') {
-				ctx.fillStyle = '#f22';
+				ctx.fillStyle = finishPointColor;
 				ctx.fillRect(x*cellSize, y*cellSize, cellSize, cellSize);
-				ctx.fillStyle = '#222';
+				ctx.fillStyle = obstaclesColor;
 			} else {
 				ctx.fillRect(x*cellSize, y*cellSize, cellSize, cellSize);
 			}
 		}
 	}
+	
+	if(mouseOverCanvas) {
+		if (settingStartPoint) {
+			ctx.fillStyle = startPointColor;
+		} else if (settingFinishPoint) {
+			ctx.fillStyle = finishPointColor;
+		} else if (drawingMode) {
+			ctx.fillStyle = obstaclesColor;
+		} else {
+			ctx.fillStyle = backgroundColor;
+		}
+		if (calculatePosition(cellX, cellY) !== calculatePosition(startX, startY) &&
+				calculatePosition(cellX, cellY) !== calculatePosition(finishX, finishY) 
+		) {
+			ctx.fillRect(cellX*cellSize, cellY*cellSize, cellSize, cellSize);
+		}
+	}
+	
 };
 
 
@@ -410,24 +479,101 @@ function calculatePosition(x, y) {
 
 function renderStep() {
 	for (var i in openList) {
-		ctx.fillStyle = '#90C3D4';
-		ctx.fillRect(openList[i].x*cellSize+3, openList[i].y*cellSize+3, cellSize-6, cellSize-6);
+		ctx.fillStyle = openElementColor;
+		ctx.fillRect(openList[i].x*cellSize+1, openList[i].y*cellSize+1, cellSize-2, cellSize-2);
+		drawPointer(openList[i]);
 	}
 	
 	for (var i in closedList) {	
-		ctx.fillStyle = '#B3B3B3';
-		ctx.fillRect(closedList[i].x*cellSize+3, closedList[i].y*cellSize+3, cellSize-6, cellSize-6);
+		if(closedList[i].position !== calculatePosition(startX, startY)) {
+		ctx.fillStyle = closedElementColor;
+		ctx.fillRect(closedList[i].x*cellSize+1, closedList[i].y*cellSize+1, cellSize-2, cellSize-2);
+		drawPointer(closedList[i]);
+		}
+	}
+
+	//ctx.fillStyle = pathColor;
+	//ctx.fillRect(current.x*cellSize+1, current.y*cellSize+1, cellSize-2, cellSize-2);
+	ctx.strokeStyle = pathColor;
+	ctx.strokeRect(current.x*cellSize+2, current.y*cellSize+2, cellSize-4, cellSize-4);
+	
+	drawPointer(current);
+}
+
+
+function drawPointer(node) {
+	ctx.strokeStyle = pointerColor;
+
+	var difference = 0;
+	
+	if(node.parent !== null) {
+		ctx.beginPath();
+		ctx.arc(node.x*cellSize+cellSize/2, node.y*cellSize+cellSize/2, cellSize/6, 0, 2 * Math.PI, false);
+		ctx.stroke();
+		
+		difference = node.parent - node.position;
 	}
 	
-	ctx.fillStyle = '#14F500';
-	ctx.fillRect(current.x*cellSize+5, current.y*cellSize+5, cellSize-10, cellSize-10);
+	switch(difference) {
+		case -width/cellSize-1:
+			ctx.beginPath();
+			ctx.moveTo((node.x + 1/2) * cellSize, (node.y + 1/2) * cellSize);
+			ctx.lineTo((node.x + 1/5) * cellSize, (node.y + 1/5) * cellSize);
+			ctx.stroke();
+			break;
+		case -width/cellSize:
+			ctx.beginPath();
+			ctx.moveTo((node.x + 1/2) * cellSize, (node.y + 1/2) * cellSize);
+			ctx.lineTo((node.x + 1/2) * cellSize, (node.y + 1/5) * cellSize);
+			ctx.stroke();
+			break;
+		case -width/cellSize+1:
+			ctx.beginPath();
+			ctx.moveTo((node.x + 1/2) * cellSize, (node.y + 1/2) * cellSize);
+			ctx.lineTo((node.x + 3/4) * cellSize, (node.y + 1/4) * cellSize);
+			ctx.stroke();
+			break;
+		case -1:
+			ctx.beginPath();
+			ctx.moveTo((node.x + 1/2) * cellSize, (node.y + 1/2) * cellSize);
+			ctx.lineTo((node.x + 1/5) * cellSize, (node.y + 1/2) * cellSize);
+			ctx.stroke();
+			break;
+		case 1:
+			ctx.beginPath();
+			ctx.moveTo((node.x + 1/2) * cellSize, (node.y + 1/2) * cellSize);
+			ctx.lineTo((node.x + 4/5) * cellSize, (node.y + 1/2) * cellSize);
+			ctx.stroke();
+			break;
+		case width/cellSize-1:
+			ctx.beginPath();
+			ctx.moveTo((node.x + 1/2) * cellSize, (node.y + 1/2) * cellSize);
+			ctx.lineTo((node.x + 1/4) * cellSize, (node.y + 3/4) * cellSize);
+			ctx.stroke();
+			break;
+		case width/cellSize:
+			ctx.beginPath();
+			ctx.moveTo((node.x + 1/2) * cellSize, (node.y + 1/2) * cellSize);
+			ctx.lineTo((node.x + 1/2) * cellSize, (node.y + 4/5) * cellSize);
+			ctx.stroke();
+			break;
+		case width/cellSize+1:
+			ctx.beginPath();
+			ctx.moveTo((node.x + 1/2) * cellSize, (node.y + 1/2) * cellSize);
+			ctx.lineTo((node.x + 3/4) * cellSize, (node.y + 3/4) * cellSize);
+			ctx.stroke();
+			break;
+		default:
+			break;
+	}
 }
 
 
 // rendering each step of searching for right path
+/*
 function renderStep_old() {
 	ctx.fillStyle = '#222';
-	ctx.strokeStyle = '#ddd';
+	ctx.strokeStyle = gridColor;
 	ctx.clearRect(0, 0, width, height);
 	
 	for (var y = 0; y < cellYCount; y++) {
@@ -435,23 +581,23 @@ function renderStep_old() {
 			if (matrix[y][x] === 0) {
 				ctx.strokeRect(x*cellSize, y*cellSize, cellSize, cellSize);
 			} else if (matrix[y][x] === 'S') {		
-				ctx.fillStyle = '#00d';
+				ctx.fillStyle = startPointColor;
 				ctx.fillRect(x*cellSize, y*cellSize, cellSize, cellSize);
-				ctx.fillStyle = '#222';
+				ctx.fillStyle = obstaclesColor;
 			} else if (matrix[y][x] === 'F') {
-				ctx.fillStyle = '#f22';
+				ctx.fillStyle = finishPointColor;
 				ctx.fillRect(x*cellSize, y*cellSize, cellSize, cellSize);
-				ctx.fillStyle = '#222';
+				ctx.fillStyle = obstaclesColor;
 			} else {
 				ctx.fillRect(x*cellSize, y*cellSize, cellSize, cellSize);
 			}
 		}
 	}
 	
-	ctx.strokeStyle = '#0d0';
+	ctx.strokeStyle = currentElementColor;
 	ctx.strokeRect(current.x*cellSize, current.y*cellSize, cellSize, cellSize);
 	ctx.fillText('Cumulated G: ' + cumulatedGCost, current.x*cellSize + 2, current.y*cellSize + 60);
-	ctx.strokeStyle = '#ddd';
+	ctx.strokeStyle = gridColor;
 
 	for (var i in openList) {	
 		ctx.fillStyle = '#999';
@@ -461,7 +607,7 @@ function renderStep_old() {
 		ctx.fillText('H: ' + openList[i].hCost, openList[i].x*cellSize + 2, openList[i].y*cellSize + 36);
 		ctx.fillText('parent: ' + openList[i].parent, openList[i].x*cellSize + 2, openList[i].y*cellSize + 48);
 		ctx.fillText('OPEN', openList[i].x*cellSize + 2, openList[i].y*cellSize + 95);
-		ctx.fillStyle = '#222';
+		ctx.fillStyle = gridColor;
 	}
 	
 	for (var i in closedList) {	
@@ -472,10 +618,11 @@ function renderStep_old() {
 		ctx.fillText('H: ' + closedList[i].hCost, closedList[i].x*cellSize + 2, closedList[i].y*cellSize + 36);
 		ctx.fillText('parent: ' + closedList[i].parent, closedList[i].x*cellSize + 2, closedList[i].y*cellSize + 48);
 		ctx.fillText('CLOSED', closedList[i].x*cellSize + 2, closedList[i].y*cellSize + 95);
-		ctx.fillStyle = '#222';
+		ctx.fillStyle = gridColor;
 	}
 	
 }
+*/
 
 
 function addNeighborsToOpenList(node) {
@@ -554,9 +701,16 @@ function findNodeWithLowestFCost(position) {
 
 // tracing back the path from start to finish
 function drawPath() {
-	renderFrame();
+	//renderFrame();
+	
+	ctx.fillStyle = startPointColor;
+	ctx.fillRect(startX*cellSize, startY*cellSize, cellSize, cellSize);
 
-	ctx.strokeStyle = '#0d0';
+	ctx.fillStyle = finishPointColor;
+	ctx.fillRect(finishX*cellSize, finishY*cellSize, cellSize, cellSize);
+	
+	ctx.strokeStyle = pathColor;
+	ctx.lineWidth = 3;
 	ctx.beginPath();
 	ctx.moveTo((finishX + 1/2) * cellSize, (finishY + 1/2) * cellSize);
 
@@ -569,7 +723,8 @@ function drawPath() {
 	}
 	
 	ctx.stroke();
-	ctx.strokeStyle = '#ddd';
+	ctx.strokeStyle = gridColor;
+	ctx.lineWidth = 1;
 }
 
 
