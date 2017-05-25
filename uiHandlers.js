44052f440f5091ad0f7d2e01ff87a25d8497d93c
Michal <m1ch4l88@gmail.com>
2017-05-25 07:13:01 +0200

    Comments

--- uiHandlers.js
+++ uiHandlers.js
@@ -1,31 +1,38 @@
-
+// if mouse entered canvas
 function canvasMouseEnterHandler() {
 	mouseOverCanvas = true;
 }
 
 
 
+// if mouse left canvas
 function canvasMouseLeaveHandler() {
 	mouseOverCanvas = false;
 }
 
 
 
+// if mouse button pressed
 function canvasMouseDownHandler(e) {
 	if(!searching) {
 		mousePressed = true;
+		
+		// calculating mouse position and map's cell based on that position
 		elementRect = canvas.getBoundingClientRect();
 		mouseX = e.pageX - elementRect.left;
 		mouseY = e.pageY - elementRect.top;
 		cellX = Math.floor(mouseX / cellSize);
 		cellY = Math.floor(mouseY / cellSize);
 		
+		// setting start point
 		if (settingStartPoint && matrix[cellY][cellX] !== 'F') {
 			matrix[startY][startX] = 0;
 			startX = cellX;
 			startY = cellY;
 			matrix[startY][startX] = 'S';
 			settingStartPoint = false;
+			
+			// enabling buttons and updating app message
 			infoText = "Draw obstacles and click <strong>Find path</strong>.";
 			$('#resetButton').attr('disabled', false);
 			$('#resetButton').removeClass('disabledButton');
@@ -34,12 +41,14 @@ function canvasMouseDownHandler(e) {
 			$('#setFinishButton').attr('disabled', false);
 			$('#setFinishButton').removeClass('disabledButton');
 			
-		} else if (settingFinishPoint && matrix[cellY][cellX] !== 'S') {
+		} else if (settingFinishPoint && matrix[cellY][cellX] !== 'S') { // setting finish point
 			matrix[finishY][finishX] = 0;
 			finishX = cellX;
 			finishY = cellY;
 			matrix[finishY][finishX] = 'F';
 			settingFinishPoint = false;
+			
+			// enabling buttons and updating app message
 			infoText = "Draw obstacles and click <strong>Find path</strong>.";
 			$('#resetButton').attr('disabled', false);
 			$('#resetButton').removeClass('disabledButton');
@@ -48,13 +57,14 @@ function canvasMouseDownHandler(e) {
 			$('#setStartButton').attr('disabled', false);
 			$('#setStartButton').removeClass('disabledButton');
 			
-		} else if (matrix[cellY][cellX] === 0 && drawingMode) {
+		} else if (matrix[cellY][cellX] === 0 && drawingMode) { // setting obstacle
 			matrix[cellY][cellX] = 1;
-		} else if (matrix[cellY][cellX] === 1 && !drawingMode) {
+		} else if (matrix[cellY][cellX] === 1 && !drawingMode) { // removing obstacle
 			matrix[cellY][cellX] = 0;
 		}
 	}
 	
+	// when search is completed and the canvas is clicked, drawing is enabled again
 	if (!searching && disableDrawing) {
 		disableDrawing = false;
 		clearInterval(interval);
@@ -65,19 +75,23 @@ function canvasMouseDownHandler(e) {
 
 
 
+// if mouse button up
 function canvasMouseUpHandler() {
 	mousePressed = false;
 }
 
 
 
+// if mouse moved
 function canvasMouseMoveHandler(e) {
+	// calculating mouse position and map's cell based on that position
 	elementRect = canvas.getBoundingClientRect();
 	mouseX = e.pageX - elementRect.left;
 	mouseY = e.pageY - elementRect.top;
 	cellX = Math.floor(mouseX / cellSize);
 	cellY = Math.floor(mouseY / cellSize);
 	
+	// adding or removing obstacles (based on drawing mode)
 	if(mousePressed) {
 		if (matrix[cellY][cellX] === 0 && drawingMode) {
 			matrix[cellY][cellX] = 1;
@@ -89,6 +103,7 @@ function canvasMouseMoveHandler(e) {
 
 
 
+// checking or unchecking "cutting through corners" checkbox
 function cuttingCornersCheckboxHandler() {
 	allowCuttingCorders = !allowCuttingCorders;
 	if (allowCuttingCorders) {
@@ -100,6 +115,7 @@ function cuttingCornersCheckboxHandler() {
 
 
 
+// checking or unchecking "show algorithm step" checkbox and showing / hiding additional options
 function showStepsCheckboxHandler() {
 	showSteps = !showSteps;
 	if (showSteps) {
@@ -113,11 +129,13 @@ function showStepsCheckboxHandler() {
 
 
 
+// selecting drawing mode
 function drawOptionHandler() {
 	$('#removeOption').removeClass('activeOption');
 	$('#drawOption').addClass('activeOption');
 	drawingMode = true;
 	
+	// if drawing was disabled during path-finding, enabling it again
 	if(disableDrawing && !searching) {
 		clearInterval(interval);
 		interval = setInterval(renderFrame, 40);
@@ -126,11 +144,13 @@ function drawOptionHandler() {
 
 
 
+// selecting removing mode
 function removeOptionHandler() {
 	$('#drawOption').removeClass('activeOption');
 	$('#removeOption').addClass('activeOption');
 	drawingMode = false;
 	
+	// if drawing was disabled during path-finding, enabling it again
 	if(disableDrawing && !searching) {
 		clearInterval(interval);
 		interval = setInterval(renderFrame, 40);
@@ -139,6 +159,7 @@ function removeOptionHandler() {
 
 
 
+// selecting slow algorithm visualisation
 function slowOptionHandler() {
 	$('.speedOptionDiv').removeClass('activeOption');
 	$('#slowOption').addClass('activeOption');
@@ -147,6 +168,7 @@ function slowOptionHandler() {
 
 
 
+// selecting medium algorithm visualisation
 function mediumOptionHandler() {
 	$('.speedOptionDiv').removeClass('activeOption');
 	$('#mediumOption').addClass('activeOption');
@@ -155,6 +177,7 @@ function mediumOptionHandler() {
 
 
 
+// selecting fast algorithm visualisation
 function fastOptionHandler() {
 	$('.speedOptionDiv').removeClass('activeOption');
 	$('#fastOption').addClass('activeOption');
@@ -163,6 +186,7 @@ function fastOptionHandler() {
 
 
 
+// if "set start point" button has been pressed, entering "setting start point" mode and disabling other buttons
 function setStartButtonHandler() {
 	settingStartPoint = true;
 	infoText = "Click on the map to set start point.";
@@ -174,6 +198,7 @@ function setStartButtonHandler() {
 	$('#setFinishButton').attr('disabled', true);
 	$('#setFinishButton').addClass('disabledButton');
 	
+	// if drawing was disabled during path-finding, enabling it again
 	if(disableDrawing) {
 		clearInterval(interval);
 		interval = setInterval(renderFrame, 40);
@@ -182,6 +207,7 @@ function setStartButtonHandler() {
 
 
 
+// if "set finish point" button has been pressed, entering "setting finish point" mode and disabling other buttons
 function setFinishButtonHandler() {
 	settingFinishPoint = true;
 	infoText = "Click on the map to set finish point.";
@@ -193,6 +219,7 @@ function setFinishButtonHandler() {
 	$('#setStartButton').attr('disabled', true);
 	$('#setStartButton').addClass('disabledButton');
 	
+	// if drawing was disabled during path-finding, enabling it again
 	if(disableDrawing) {
 		clearInterval(interval);
 		interval = setInterval(renderFrame, 40);
@@ -201,6 +228,7 @@ function setFinishButtonHandler() {
 
 
 
+// if "reset" button has been pressed, map is cleared and the algorithm is restarted
 function resetButtonHandler() {
 	startX = 1;
 	startY = 1;
@@ -224,11 +252,15 @@ function resetButtonHandler() {
 
 
 
+// if "reset" button has been pressed, the path from start point to finish point is calculated
 function findButtonHandler() {
+	
+	// locking the canvas, so drawing is impossible during searching
 	searching = true;
 	disableDrawing = true;
 	infoText = 'Searching...';
 	
+	// also locking some of the buttons
 	$('#setFinishButton').attr('disabled', true);
 	$('#setFinishButton').addClass('disabledButton');
 	$('#setStartButton').attr('disabled', true);
@@ -236,51 +268,79 @@ function findButtonHandler() {
 	$('#resetButton').attr('disabled', true);
 	$('#resetButton').addClass('disabledButton');
 
+	// displaying current state of the map (to remove the old path if "Find path" button has been presses again)
 	renderFrame();
 	
+	// stopping map refreshing (no needed during path-finding process)
 	clearInterval(interval);
+	
+	// initiating the search algorithm
 	searchFailed = false;
 	openList = {};
 	closedList = {};
 	cumulatedGCost = 0;
-	
 	grid = makeGrid(matrix);
 	
+	// adding start point to the "open list"
 	current = grid[calculatePosition(startX, startY)];	
 	current.hCost = Math.abs(current.x - finishX) + Math.abs(current.y - finishY);
 	current.gCost = 0;
 	current.fCost = current.hCost + current.gCost;
-	
 	openList[calculatePosition(startX, startY)] = current;
 	current.onOpenList = true;
 	
+	// adding its neighbors to the "open list" (if they can be added) / updating the neighbors that are already added there
 	addNeighborsToOpenList(current);
 	
+	// if showing algorithm steps is enabled, draw the first step. Also refresh the start point (so its always clearly visible)
 	if (showSteps) {
-		var searchingForPath = setInterval(function() {
+		renderStep();
+		ctx.fillStyle = startPointColor;
+		ctx.fillRect(startX*cellSize, startY*cellSize, cellSize, cellSize);
+	}
+	
+	// further algorithm steps
+	if (showSteps) { // when displaying steps is selected
+		var searchingForPath = setInterval(function() { // repeatedly calculating and drawing the steps 
+		
+			// calculating next steps if searching still possible and finish point not reached yet
 			if (current.position !== calculatePosition(finishX, finishY) && !searchFailed) {
+			
+				// removing current node from the "open list" and adding it to the "closed list"
 				delete openList[current.position];
 				current.onOpenList = false;
 				closedList[current.position] = current;
-				current.onClosedList = true;		
-				addNeighborsToOpenList(current);			
+				current.onClosedList = true;	
+				
+				// adding its neighbors to the "open list" (if they can be added) / updating the neighbors that are already added there
+				addNeighborsToOpenList(current);	
+				
+				// if no nodes left on the "open list", search failed
 				if (Object.keys(openList).length === 0) {
 					searchFailed = true;
 				} else {
+					// else find a node that is closer to the finish point than current one (based on F cost heuristic)
 					current = findNodeWithLowestFCost(current.position);
 				}	
+				
+				// drawing current step
 				renderStep();
-			} else {
-				clearInterval(searchingForPath);
-					
+				
+			} else { // if current node = finish point or search failed
+				
+				// stop searching
+				clearInterval(searchingForPath); 
+				
 				if (!searchFailed) {
-					drawPath();
+					drawPath(); // draw the path if search succeeded
 					infoText = 'Path found!';
 				} else {
 					infoText = 'Path does not exist.';
 				}
 				
 				searching = false;
+				
+				// enable the buttons that was locked during searching
 				$('#setFinishButton').attr('disabled', false);
 				$('#setFinishButton').removeClass('disabledButton');
 				$('#setStartButton').attr('disabled', false);
@@ -290,28 +350,40 @@ function findButtonHandler() {
 				
 			}
 		}, animationSpeed);
-	} else {	
+	} else { // further algorithm steps when displaying steps is not selected
+	
+		// calculating next steps if searching still possible and finish point not reached yet
 		while (current.position !== calculatePosition(finishX, finishY) && !searchFailed) {
+		
+			// removing current node from the "open list" and adding it to the "closed list"
 			delete openList[current.position];	
 			current.onOpenList = false;
 			closedList[current.position] = current;
-			current.onClosedList = true;		
-			addNeighborsToOpenList(current);			
+			current.onClosedList = true;	
+			
+			// adding its neighbors to the "open list" (if they can be added) / updating the neighbors that are already added there
+			addNeighborsToOpenList(current);	
+			
+			// if no nodes left on the "open list", search failed
 			if (Object.keys(openList).length === 0) {
 				searchFailed = true;
 			} else {
+				// else find a node that is closer to the finish point than current one (based on F cost heuristic)
 				current = findNodeWithLowestFCost(current.position);
 			}		
 		}
 		
+		// draw the path if search succeeded
 		if (!searchFailed) {
-			drawPath();
+			drawPath(); // draw the path if search succeeded
 			infoText = 'Path found!';
 		} else {
 			infoText = 'Path does not exist.';
 		}
 		
 		searching = false;
+		
+		// enable the buttons that was locked during searching
 		$('#setFinishButton').attr('disabled', false);
 		$('#setFinishButton').removeClass('disabledButton');
 		$('#setStartButton').attr('disabled', false);
